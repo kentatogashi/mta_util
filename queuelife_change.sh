b8643aaf1d5c2ea892b192271bb6572eb5460cc0
Kenta Togashi <4276550+kentatogashi@users.noreply.github.com>
2018-07-05 11:11:04 +0900

    value with curly bracket.

--- queuelife_change.sh
+++ queuelife_change.sh
@@ -1,16 +1,16 @@
 #!/usr/local/bin/bash
 PATH=/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-PROGNAME=$(basename $0)
+PROGNAME=$(basename ${0})
 
 function usage {
-    echo "$PROGNAME is a tool for change queue lifetime.
+    echo "${PROGNAME} is a tool for change queue lifetime.
 
 Usage:
-    $PROGNAME <mta> <lifetime>
+    ${PROGNAME} <mta> <lifetime>
 
 Arg:
-    <mta>:  qmail | postfix
-    <lifetime>: $LIFETIMES"
+    <mta>:  qmail|postfix
+    <lifetime>: ${LIFETIMES}"
     exit 1
 }
 
@@ -23,49 +23,49 @@ function settings {
 }
 
 function err_exit {
-    echo $@ 1>&2
+    echo ${@} 1>&2
     exit 1
 }
 
 function backup {
-    local f=$1
-    if [ -f $f ]
+    local f=${1}
+    if [ -f ${f} ]
     then
-        cp -p $f $f.$(date +"%Y%m%d") || err_exit "failed to backup."
+        cp -p ${f} ${f}.$(date +"%Y%m%d") || err_exit "failed to backup."
     else
-        err_exit "not found $f for backup."
+        err_exit "not found ${f} for backup."
     fi
 }
 
 function has_postfix {
-    grep -q '^postfix:' $PASSWD
-    return $?
+    grep -q '^postfix:' ${PASSWD}
+    return ${?}
 }
 
 # qmail is running on tcpserver
 function change_qmail_lifetime {
-    if [ -f $QMAIL_CONF ]
+    if [ -f ${QMAIL_CONF} ]
     then
-        CURRENT_LIFETIME=$(cat $QMAIL_CONF)
-        echo "Change queuelifetime? [$CURRENT_LIFETIME => $LIFETIME] [ yes | no ]"
+        CURRENT_LIFETIME=$(cat ${QMAIL_CONF})
+        echo "Change queuelifetime? [${CURRENT_LIFETIME} => ${LIFETIME}] [ yes | no ]"
         read ans
         echo
         [ "${ans}" != "yes" ] && err_exit "abort."
-        backup $QMAIL_CONF
-        echo $LIFETIME > $QMAIL_CONF
-        pid1=`svstat $SERVICE_QMAIL | sed -ne 's/.*pid \(.*\)).*$/\1/p'`
-        svc -d $SERVICE_QMAIL
+        backup ${QMAIL_CONF}
+        echo ${LIFETIME} > ${QMAIL_CONF}
+        pid1=`svstat ${SERVICE_QMAIL} | sed -ne 's/.*pid \(.*\)).*$/\1/p'`
+        svc -d ${SERVICE_QMAIL}
         killall qmail-remote || :
         sleep 3
-        svc -u $SERVICE_QMAIL
-        pid2=`svstat $SERVICE_QMAIL | sed -ne 's/.*pid \(.*\)).*$/\1/p'`
-        if [[ -z "$pid1" ]] || [[ -z "$pid2" ]] || [[ $pid1 -eq $pid2 ]]
+        svc -u ${SERVICE_QMAIL}
+        pid2=`svstat ${SERVICE_QMAIL} | sed -ne 's/.*pid \(.*\)).*$/\1/p'`
+        if [[ -z "${pid1}" ]] || [[ -z "${pid2}" ]] || [[ ${pid1} -eq ${pid2} ]]
         then
             err_exit 'qmail fails to restart'
         fi
-        svstat $SERVICE_QMAIL
+        svstat ${SERVICE_QMAIL}
     else
-        err_exit "not found $QMAIL_CONF."
+        err_exit "not found ${QMAIL_CONF}."
     fi
 }
 
@@ -74,13 +74,13 @@ function change_postfix_lifetime {
     then
         CURRENT_LIFETIME="$(postconf | grep lifetime)"
         echo "Change queuelifetime? ["
-        echo "$CURRENT_LIFETIME => ${LIFETIME}s] [ yes | no ]"
+        echo "${CURRENT_LIFETIME} => ${LIFETIME}s] [ yes | no ]"
         read ans
         echo
         [ "${ans}" != "yes" ] && err_exit "abort."
-        backup $POSTFIX_CONF
+        backup ${POSTFIX_CONF}
         sed -i "" -e "s/^bounce_queue_lifetime = \(.*\)$/bounce_queue_lifetime = ${LIFETIME}s/" \
-                  -e "s/maximal_queue_lifetime = \(.*\)/maximal_queue_lifetime = ${LIFETIME}s/" $POSTFIX_CONF
+                  -e "s/maximal_queue_lifetime = \(.*\)/maximal_queue_lifetime = ${LIFETIME}s/" ${POSTFIX_CONF}
         postfix check || err_exit "main.cf has syntax error."
         postfix reload || err_exit "failed to reload postfix."
     else
@@ -89,19 +89,16 @@ function change_postfix_lifetime {
 }
 
 settings
+[ -z "${1}" ] && usage
+[ -z "${2}" ] && usage
+MTA=${1}
+LIFETIME=${2}
+[ $(echo ${LIFETIME}| egrep "^(${LIFETIMES})$") ] || usage
 
-[ -z "$1" ] && usage
-[ -z "$2" ] && usage
-
-MTA=$1
-LIFETIME=$2
-
-[ $(echo $LIFETIME | egrep "^($LIFETIMES)$") ] || usage
-
-if [ $MTA = qmail ]
+if [ ${MTA} = qmail ]
 then
     change_qmail_lifetime
-elif [ $MTA = postfix ]
+elif [ ${MTA} = postfix ]
 then
     change_postfix_lifetime
 else
